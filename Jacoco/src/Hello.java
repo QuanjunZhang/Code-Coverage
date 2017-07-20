e071a5dee78eb6ae3cffcd52168e4852fa80e866
OHCE <1499377793zqj@gmail.com>
2017-07-20 21:51:51 +0800

    jacoco

---
+++ Jacoco/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public Hello(){
+		int rand=(int)(Math.random()*100);
+		if(rand%2==0){
+			System.out.println("Hi,0");
+		}else{
+			System.out.println("Hi,1");
+		}
+		System.out.println("End");
+	}
+}
---
+++ Jacoco/src/HelloTest.java
@@ -0,0 +1,7 @@
+
+public class HelloTest {
+
+public static void main(String[] args)  {
+		Hello h=new Hello();
+		}
+}
